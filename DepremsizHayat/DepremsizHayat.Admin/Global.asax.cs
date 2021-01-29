5dc3deedcafdcf55a2046f9fba4b3cc21ab3dac7
YigitGok <ytgokk@gmail.com>
2021-01-29 21:23:11 +0300

    HomeController başlandı

--- DepremsizHayat/DepremsizHayat.Admin/Global.asax.cs
+++ DepremsizHayat/DepremsizHayat.Admin/Global.asax.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using DepremsizHayat.App.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace DepremsizHayat.Admin
@@ -13,9 +13,8 @@ namespace DepremsizHayat.Admin
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutofacSettings.Run();
         }
     }
 }
