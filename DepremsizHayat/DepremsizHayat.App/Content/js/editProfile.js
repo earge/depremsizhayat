571c878cda4ff12b63d6dff716b13e692d6f4554
ridvanonal <ridvanonal@icloud.com>
2021-01-31 16:33:43 +0300

    .

--- DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
+++ DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
@@ -2,23 +2,13 @@
 let newSurname = document.querySelector("[name='newSurname']")
 let oldPassword = document.querySelector("[name='oldPassword']")
 let newPassword = document.querySelector("[name='newPassword']")
-newName.value = ""
-newSurname.value = ""
-newPassword.value = ""
-oldPassword.value = ""
 
-function getNameSurname() {
-    $.ajax({
-        url: "NameSurnameJson",
-        type: "post",
-        success: function (data) {
-            newName.value = data.Name
-            newSurname.value = data.Surname
-        }
-    })
-}
+newName.onfocus = function() { newName.removeAttribute("readonly") }
+newSurname.onfocus = function () { newSurname.removeAttribute("readonly") }
+newPassword.onfocus = function () { newPassword.removeAttribute("readonly") }
+oldPassword.onfocus = function () { oldPassword.removeAttribute("readonly") }
 
-getNameSurname()
+
 
 
 
