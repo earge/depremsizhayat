283c2e6d2eaec22a30d5d6e42d24ac9a974b19dd
ridvanonal <ridvanonal@icloud.com>
2021-02-09 15:25:49 +0300

    Edit Profil Sayfası Oluşturuldu.

--- DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
+++ DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
@@ -1,38 +1,55 @@
-﻿let newName = document.querySelector("[name='newName']")
-let newSurname = document.querySelector("[name='newSurname']")
-let oldPassword = document.querySelector("[name='oldPassword']")
-let newPassword = document.querySelector("[name='newPassword']")
-let newMail = document.querySelector("[name='newMail']")
+﻿let newName = document.querySelector("input[name='newName']")
+let newSurname = document.querySelector("input[name='newSurname']")
+let newPassword = document.querySelector("input[name='newPassword']")
+let newPasswordAgain = document.querySelector("input[name='newPasswordAgain']")
 
 newName.onfocus = function () { newName.removeAttribute("readonly") }
 newSurname.onfocus = function () { newSurname.removeAttribute("readonly") }
 newPassword.onfocus = function () { newPassword.removeAttribute("readonly") }
-oldPassword.onfocus = function () { oldPassword.removeAttribute("readonly") }
-newMail.onfocus = function () { newMail.removeAttribute("readonly") }
+newPasswordAgain.onfocus = function () { newPasswordAgain.removeAttribute("readonly") }
 
 
 document.querySelector("#edit").addEventListener("click", function (event) {
+    let ajaxValid = false
+    let passwordBox
+    if (newPassword.value != "") {
+        if (newPasswordAgain.value == newPassword.value) {
+            document.querySelector(".extraSpan").classList.add("hidden")
+            passwordBox = newPassword.value
+            ajaxValid = true
+        } else {
+            document.querySelector(".extraSpan").classList.remove("hidden")
+            ajaxValid = false
+        }
+    } else {
+        passwordBox = null
+        ajaxValid = true
+    }
+
     var data =
     {
         Name: newName.value,
         Surname: newSurname.value,
-        Mail: newMail.value
+        Password: passwordBox,
     }
-    event.target.classList.add("loading")
-    event.target.disabled = true
-    $.ajax({
-        data: data,
-        url: "EditProfile",
-        type: "POST",
-        success: function (data) {
-            event.target.classList.remove("loading")
-            if (data.Status) {
-                event.target.disabled = true
-                setTimeout(function () { location.reload() }, 1000)
+
+    if (ajaxValid) {
+        event.target.classList.add("loading")
+        event.target.disabled = true
+        $.ajax({
+            data: data,
+            url: "EditProfile",
+            type: "POST",
+            success: function (data) {
+                event.target.classList.remove("loading")
+                if (data.Status) {
+                    event.target.disabled = true
+                    setTimeout(function () { location.reload() }, 1000)
+                }
+                else { event.target.disabled = false }
+                ResponseMessage("editJsonInfo", data)
             }
-            else { event.target.disabled = false }
-            ResponseMessage("editJsonInfo",data)
-        }
-    })
+        })
+    }
 })
 
