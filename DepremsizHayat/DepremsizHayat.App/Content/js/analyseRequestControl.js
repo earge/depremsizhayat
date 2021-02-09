0f734ca37b75da8dc2e666ab3affec4fdb44633a
ridvanonal <ridvanonal@icloud.com>
2021-02-09 23:39:52 +0300

    Edit Page ve Analyse Request Sayfalarının kontrolleri sağlandı.

--- DepremsizHayat/DepremsizHayat.App/Content/js/analyseRequestControl.js
+++ DepremsizHayat/DepremsizHayat.App/Content/js/analyseRequestControl.js
@@ -24,23 +24,59 @@ requestButton.addEventListener("click", function (event) {
         sendingFiles.append(images[i].name, images[i])
     }
 
-    $.ajax({
-        url: "/Home/SendAnalyseRequest",
-        type: "POST",
-        processData: false,
-        contentType: false,
-        data: sendingFiles,
-        success: function () {
-            debugger;
-        },
-        error: function () {
-            debugger;
-        }
-    })
+    if (images.length == 0) { console.log("En az bir resim yüklemelisiniz") }
+    else if (year.value == -1) { console.log("Yapım Yılı Seçiniz") }
+    else if (floor.value == -1) { console.log("Kat Sayısını Seçiniz") }
+    else if (country.value == -1) { console.log("Ülke Seçiniz") }
+    else if (district.value == -1) { console.log("Şehir Seçiniz") }
+    else if (address.value.trim().length == 0) { console.log("Adres Giriniz") }
+    else if (note.value.trim().length == 0) { console.log("Açıklama Giriniz") }
+    else {
+        $.ajax({
+            url: "/Home/SendAnalyseRequest",
+            type: "POST",
+            beforeSend: function () {
+                event.target.classList.add("loading")
+                event.target.disabled = true
+            },
+            processData: false,
+            contentType: false,
+            data: sendingFiles,
+            xhr: function () {
+                var xhr = new window.XMLHttpRequest();
+                //Download progress
+                xhr.addEventListener("progress", function (evt) {
+                    console.log(evt.lengthComputable);
+                    if (evt.lengthComputable) {
+                        var percentComplete = evt.loaded / evt.total;
+                        console.log(Math.round(percentComplete * 100) + "%")
+                    }
+                }, false);
+                return xhr;
+            },
+            success: function (data) {
+                event.target.classList.remove("loading")
+                event.target.disabled = false
+                ResponseMessage("analyseJsonInfo", data)
+                resetForm()
+            },
+            
+        })
+    }
 })
 
-//if (images.length < 0) { console.log("En az bir resim yüklemelisiniz") }
-//else if (year.value == -1) { console.log("Yapım Yılı Seçiniz") }
-//else if()
+function resetForm() {
+    document.querySelector("#phone1").value = ""
+    document.querySelector("#phone2").value = ""
+    document.querySelector("#year").value = -1
+    document.querySelector("#floor").value = -1
+    document.querySelector("#country").value = -1
+    document.querySelector("#district").value=-1
+    document.querySelector("#address").value=""
+    document.querySelector("#note").value = ""
+    images = []
+    document.querySelectorAll(".imagesBox>div>div").forEach(item=>item.remove())
+    document.querySelector(".imagesBox").classList.add("hidden")
+}
 
 
