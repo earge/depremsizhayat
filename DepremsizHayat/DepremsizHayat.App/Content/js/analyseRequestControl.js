ef18bb9433164b19bc3bd90aa20679db705907f8
ridvanonal <ridvanonal@icloud.com>
2021-02-09 22:37:40 +0300

    .

--- DepremsizHayat/DepremsizHayat.App/Content/js/analyseRequestControl.js
+++ DepremsizHayat/DepremsizHayat.App/Content/js/analyseRequestControl.js
@@ -1,24 +1,24 @@
 ﻿let requestButton = document.querySelector("#request")
 requestButton.addEventListener("click", function (event) {
-    let phone1 = document.querySelector("#phone1").value
-    let phone2 = document.querySelector("#phone2").value
-    let year = document.querySelector("#year").value
-    let floor = document.querySelector("#floor").value
-    let country = document.querySelector("#country").value
-    let district = document.querySelector("#district").value
-    let address = document.querySelector("#address").value
-    let note = document.querySelector("#note").value
+    let phone1 = document.querySelector("#phone1")
+    let phone2 = document.querySelector("#phone2")
+    let year = document.querySelector("#year")
+    let floor = document.querySelector("#floor")
+    let country = document.querySelector("#country")
+    let district = document.querySelector("#district")
+    let address = document.querySelector("#address")
+    let note = document.querySelector("#note")
 
     let sendingFiles = new FormData()
 
-    sendingFiles.append("phone1", phone1)
-    sendingFiles.append("phone2", phone2)
-    sendingFiles.append("year", year)
-    sendingFiles.append("floor", floor)
-    sendingFiles.append("country", country)
-    sendingFiles.append("district", district)
-    sendingFiles.append("address", address)
-    sendingFiles.append("note", note)
+    sendingFiles.append("phone1", phone1.value)
+    sendingFiles.append("phone2", phone2.value)
+    sendingFiles.append("year", year.value)
+    sendingFiles.append("floor", floor.value)
+    sendingFiles.append("country", country.value)
+    sendingFiles.append("district", district.value)
+    sendingFiles.append("address", address.value)
+    sendingFiles.append("note", note.value)
 
     for (i = 0; i < images.length; i++) {
         sendingFiles.append(images[i].name, images[i])
@@ -39,6 +39,8 @@ requestButton.addEventListener("click", function (event) {
     })
 })
 
-
+//if (images.length < 0) { console.log("En az bir resim yüklemelisiniz") }
+//else if (year.value == -1) { console.log("Yapım Yılı Seçiniz") }
+//else if()
 
 
