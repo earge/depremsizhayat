0f75b772e7d09b78c70c6a11ceb7d5522d9b1429
ridvanonal <ridvanonal@icloud.com>
2021-02-09 20:46:35 +0300

    Edit Page

--- DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
+++ DepremsizHayat/DepremsizHayat.App/Content/js/editProfile.js
@@ -1,56 +1,81 @@
-﻿let newName = document.querySelector("input[name='newName']")
+﻿
+
+let newName = document.querySelector("input[name='newName']")
 let newSurname = document.querySelector("input[name='newSurname']")
 let newPassword = document.querySelector("input[name='newPassword']")
 let newPasswordAgain = document.querySelector("input[name='newPasswordAgain']")
+let newFIPassword = document.querySelector("#newPassword")
+let newFIName = document.querySelector("#newName")
+let newFISurname = document.querySelector("#newSurname")
+newFIName.valid = true
+newFISurname.valid = true
 
-newName.onfocus = function () { newName.removeAttribute("readonly") }
-newSurname.onfocus = function () { newSurname.removeAttribute("readonly") }
 newPassword.onfocus = function () { newPassword.removeAttribute("readonly") }
 newPasswordAgain.onfocus = function () { newPasswordAgain.removeAttribute("readonly") }
 
-
-document.querySelector("#edit").addEventListener("click", function (event) {
-    let ajaxValid = false
-    let passwordBox
-    if (newPassword.value != "") {
-        if (newPasswordAgain.value == newPassword.value) {
+function checkAgain() {
+    if (newPasswordAgain.value.trim().length != 0) {
+        if (newPassword.value == newPasswordAgain.value) {
             document.querySelector(".extraSpan").classList.add("hidden")
-            passwordBox = newPassword.value
-            ajaxValid = true
-            debugger;
+            newPasswordAgain.classList.remove("border-red")
+            return true
+
         } else {
             document.querySelector(".extraSpan").classList.remove("hidden")
-            ajaxValid = false
+            newPasswordAgain.classList.add("border-red")
+            return false
+        }
+    }
+}
+
+newPasswordAgain.addEventListener("keyup", checkAgain)
+newPassword.addEventListener("keyup", checkAgain)
+
+
+document.querySelector("#edit").addEventListener("click", function (event) {
+    if (newPassword.value.trim().length != 0) {
+        if (checkAgain()) {
+            if (newFIName.valid) {
+                if (newFISurname.valid) {
+                    ajaxForEditProfile(event) //Burası Şifreli
+                } else {
+                    newFISurname.Check()
+                }
+            } else {
+                newFIName.Check()
+            }
         }
     } else {
-        passwordBox = null
-        ajaxValid = true
-    }
+        if (newFIName.valid) {
+            if (newFISurname.valid) {
+                ajaxForEditProfile(event) //Burası Şifresiz
+            } else {
+                newFISurname.Check()
+            }
+        } else {
+            newFIName.Check()
+        }
+    }    
+})
 
+function ajaxForEditProfile(event) {
     var data =
     {
         Name: newName.value,
         Surname: newSurname.value,
-        Password: passwordBox,
+        Password: newPassword.value,
     }
-
-    if (ajaxValid) {
-        event.target.classList.add("loading")
-        event.target.disabled = true
-        $.ajax({
-            data: data,
-            url: "EditProfile",
-            type: "POST",
-            success: function (data) {
-                event.target.classList.remove("loading")
-                if (data.Status) {
-                    event.target.disabled = true
-                    setTimeout(function () { location.reload() }, 1000)
-                }
-                else { event.target.disabled = false }
-                ResponseMessage("editJsonInfo", data)
-            }
-        })
-    }
-})
-
+    event.target.classList.add("loading")
+    event.target.disabled = true
+    $.ajax({
+        data: data,
+        url: "EditProfile",
+        type: "POST",
+        success: function (data) {
+            event.target.classList.remove("loading")
+            event.target.disabled = false
+            //Array.from(document.querySelectorAll(".nameSurnameJson")).forEach(item=>item.innerHTML = )
+            ResponseMessage("editJsonInfo", data)
+        }
+    })
+}
